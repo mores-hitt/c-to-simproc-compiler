00fcdcc6b76fe1d0a70907bdec22d01b52eb351c
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2026-01-21 19:56:34 -0300

    feat(sp-cli): add stack pointer initial address to constants

--- sp-cli/const/const.h
+++ sp-cli/const/const.h
@@ -10,5 +10,6 @@ namespace sp_cli {
     inline constexpr int MEMORY_SIZE = 16;
     inline constexpr int TOTAL_AR = 4;
     inline constexpr int TOTAL_GPR = 3;
-    inline constexpr int MEMORY_PRINT_WINDOW = 20;
+    inline constexpr int MEMORY_PRINT_WINDOW = 10;
+    inline constexpr int STACK_ADDRESS = 0xF80;
 } // namespace sp_cli
