7008d765880a12243e30d6935fd512bae6b508d5
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2026-02-16 23:02:12 -0300

    feat(scc): add first AST node classes

---
+++ scc/parser/ast/ast-node.cpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <string_view>
+#include <string>
+#include <iostream>
+
+#include "ast-node.h"
+
+namespace scc
+{
+    void IntegerConstantNode::print(int depth) const {
+        std::string spaces {(depth * 2, ' ')};
+
+        std::cout << spaces << "IntegerConstant(" << m_integer << ")";
+    }
+
+    void IntegerConstantNode::expr() const {
+        std::cout << "override funciona en herencia de ExpressionNode a IntegerConstantNode\n";
+    }
+
+    void ReturnNode::print(int depth) const {
+        std::string spaces {(depth * 2, ' ')};
+    
+        std::cout << spaces << "Return(\n";
+        m_expression->print(depth+1);
+        std::cout << "\n" << spaces << ")";
+    }
+
+    void ReturnNode::stmnt() const {
+        std::cout << "override funciona en herencia de StatementNode a ReturnNode\n";
+    }
+
+    void FunctionDefinitionNode::print(int depth) const {
+        std::string spaces {(depth * 2, ' ')};
+
+        std::cout << spaces << "Function(\n"
+                  << spaces << "  name=\"" << m_name << "\",\n"
+                  << spaces << "  body=";
+        m_body->print(depth+1);
+        std::cout << spaces << ")";
+    }
+
+    void FunctionDefinitionNode::funcDef() const {
+        std::cout << "Funciona funcDef supongo lol";
+    }
+
+    void ProgramNode::print(int depth) const {
+        std::cout << "Program(\n";
+        m_functionDefinition->print(depth + 1);
+        std::cout << ")\n";
+    }
+
+    void ProgramNode::prgrm() const {
+        std::cout << "Functiona prgrm supongo lol";
+    }
+} // namespace scc
---
+++ scc/parser/ast/ast-node.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <string_view>
+#include <string>
+#include <memory>
+
+
+namespace scc
+{
+    class ASTNode {
+    protected:
+        std::string_view m_codeRef {};
+        int m_lineNumber {1};
+        int m_columnNumber {1};
+
+    public:
+
+        explicit ASTNode(std::string_view codeRef, int lineNumber, int columnNumber)
+            : m_codeRef{codeRef}
+            , m_lineNumber{lineNumber}
+            , m_columnNumber{columnNumber} {}
+
+
+        virtual ~ASTNode() = default;
+
+        ASTNode(const ASTNode&) = delete;
+        ASTNode& operator=(const ASTNode&) = delete;
+
+        ASTNode(ASTNode&&) = default;
+        ASTNode& operator=(ASTNode&&) = default;
+
+        virtual void print(int depth = 0) const = 0;
+
+        /*
+        parserVisit()?
+        codeGenVisit()?
+        generateAssembly()?
+        checkSyntax()?
+        */
+
+        std::string_view getCodeRef() const { return m_codeRef; }
+        int getLineNumber() const { return m_lineNumber; }
+        int getColumnNumber() const { return m_columnNumber; }
+    };
+
+    class ExpressionNode : public ASTNode {
+    public:
+
+        explicit ExpressionNode(std::string_view codeRef, int lineNumber, int columnNumber)
+            : ASTNode(codeRef, lineNumber, columnNumber) {}
+
+        virtual void expr() const = 0;
+    };
+
+    class IntegerConstantNode : public ExpressionNode {
+    protected:
+        int m_integer {0};
+
+    public:
+        explicit IntegerConstantNode(std::string_view codeRef, int lineNumber, int columnNumber, int integer)
+            : ExpressionNode(codeRef, lineNumber, columnNumber)
+            , m_integer(integer) {}
+        
+        void print(int depth = 0) const override;
+
+        void expr() const override;
+    };
+    
+    class StatementNode : public ASTNode {
+    public:
+        explicit StatementNode(std::string_view codeRef, int lineNumber, int columnNumber)
+            : ASTNode(codeRef, lineNumber, columnNumber) {}
+        
+        virtual void stmnt() const = 0;
+    };
+
+    class ReturnNode : public StatementNode {
+    protected:
+        std::unique_ptr<ExpressionNode> m_expression;
+
+    public:
+        
+        explicit ReturnNode(std::string_view codeRef, int lineNumber, int columnNumber, std::unique_ptr<ExpressionNode> expression)
+            : StatementNode(codeRef, lineNumber, columnNumber)
+            , m_expression(std::move(expression)) {}
+        
+        void print(int depth = 0) const override;
+
+        void stmnt() const override;
+    };
+
+    class FunctionDefinitionNode : public ASTNode {
+    protected:
+        std::string_view m_name {};
+        std::unique_ptr<StatementNode> m_body;
+
+    public:
+        FunctionDefinitionNode(std::string_view codeRef, int lineNumber, int columnNumber, std::string_view name, std::unique_ptr<StatementNode> body)
+            : ASTNode(codeRef, lineNumber, columnNumber)
+            , m_name(name)
+            , m_body(std::move(body)) {}
+
+        void print(int depth = 0) const override;
+
+        void funcDef() const;
+    };
+
+    class ProgramNode : public ASTNode {
+    protected:
+        std::unique_ptr<FunctionDefinitionNode> m_functionDefinition;
+
+    public:
+        ProgramNode(std::string_view codeRef, int lineNumber, int columnNumber, std::unique_ptr<FunctionDefinitionNode> functionDefinition)
+            : ASTNode(codeRef, lineNumber, columnNumber)
+            , m_functionDefinition(std::move(functionDefinition)) {}
+        
+        void print(int depth = 0) const override;
+
+        void prgrm() const;
+    };
+} // namespace scc
